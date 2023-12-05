8f72ba300924110d6ec0e31bcc202721212ea012
marihachi <marihachi0620@gmail.com>
2023-12-06 00:23:40 +0900

    syntax node

--- src/syntax/node.ts
+++ src/syntax/node.ts
@@ -2,3 +2,28 @@ export type Loc = {
   line: number;
   column: number;
 };
+
+export type SyntaxNode = Unit | Expression | Decl;
+
+export class Unit {
+  kind = 'Unit' as const;
+}
+
+export type Decl = FunctionDecl | VariableDecl;
+
+export class FunctionDecl {
+  kind = 'FunctionDecl' as const;
+}
+
+export class VariableDecl {
+  kind = 'VariableDecl' as const;
+}
+
+export type Expression = NumberLiteral;
+
+export class NumberLiteral {
+  kind = 'NumberLiteral' as const;
+  constructor(
+    public value: number,
+  ) {}
+}
