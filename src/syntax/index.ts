d4f94dd68d3752aed7c7c9bb8e70ee26946af2f4
marihachi <marihachi0620@gmail.com>
2023-12-06 20:51:57 +0900

    node

--- src/syntax/index.ts
+++ src/syntax/index.ts
@@ -2,5 +2,5 @@ import { Unit } from './node.js';
 
 export function parse(input: string): Unit {
   // TODO
-  return new Unit();
+  return new Unit([], { line: 1, column: 1 });
 }
--- src/syntax/node.ts
+++ src/syntax/node.ts
@@ -3,27 +3,79 @@ export type Loc = {
   column: number;
 };
 
-export type SyntaxNode = Unit | Expression | Decl;
+export type SyntaxNode = Unit | Decl | Expression | Statement;
 
 export class Unit {
   kind = 'Unit' as const;
+  constructor(
+    public decls: Decl[],
+    public loc: Loc,
+  ) {}
 }
 
 export type Decl = FunctionDecl | VariableDecl;
 
 export class FunctionDecl {
   kind = 'FunctionDecl' as const;
+  constructor(
+    public loc: Loc,
+  ) {}
 }
 
 export class VariableDecl {
   kind = 'VariableDecl' as const;
+  constructor(
+    public loc: Loc,
+  ) {}
 }
 
-export type Expression = NumberLiteral;
+export type Expression = NumberLiteral | Binary | Unary;
 
 export class NumberLiteral {
   kind = 'NumberLiteral' as const;
   constructor(
     public value: number,
+    public loc: Loc,
+  ) {}
+}
+
+export class Binary {
+  kind = 'Binary' as const;
+  constructor(
+    public mode: 'add' | 'sub' | 'mul' | 'div' | 'rem' | 'and' | 'or' | 'shl' | 'shr' | 'bitand' | 'bitor' | 'xor',
+    public left: Expression,
+    public right: Expression,
+    public loc: Loc,
+  ) {}
+}
+
+export class Unary {
+  kind = 'Unary' as const;
+  constructor(
+    public mode: 'not' | 'compl' | 'plus' | 'minus',
+    public expr: Expression,
+    public loc: Loc,
+  ) {}
+}
+
+export class If {
+  kind = 'If' as const;
+  constructor(
+    public mode: 'not' | 'compl' | 'plus' | 'minus',
+    public cond: Expression,
+    public thenExpr: Expression,
+    public elseExpr: Expression,
+  ) {}
+}
+
+export type Statement = Assign;
+
+export class Assign {
+  kind = 'Assign' as const;
+  constructor(
+    public mode: 'basic' | 'add' | 'sub' | 'mul' | 'div' | 'rem' | 'shl' | 'shr' | 'bitand' | 'bitor' | 'xor',
+    public left: Expression,
+    public right: Expression,
+    public loc: Loc,
   ) {}
 }
