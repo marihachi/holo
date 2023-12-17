97657e3109eb02cb9c6353432e3a7f78b24e7ff2
marihachi <marihachi0620@gmail.com>
2023-12-17 10:45:13 +0900

    bind

--- src/compile/bind.ts
+++ src/compile/bind.ts
@@ -4,19 +4,33 @@ import { FunctionSymbol, SemanticSymbol, VariableSymbol } from './symbol.js';
 export class Symbols {
   table: Map<SyntaxNode, SemanticSymbol> = new Map();
 
-  setSymbol(node: SyntaxNode, symbol: SemanticSymbol): void {
+  set(node: SyntaxNode, symbol: SemanticSymbol): void {
     this.table.set(node, symbol);
   }
 
-  getSymbol(node: SyntaxNode): SemanticSymbol | undefined {
+  get(node: SyntaxNode): SemanticSymbol | undefined {
     return this.table.get(node);
   }
 }
 
 class Environment {
+  table: Map<string, SemanticSymbol> = new Map();
+
   constructor(
     public parent: Environment | undefined,
   ) {}
+
+  set(name: string, symbol: SemanticSymbol): void {
+    this.table.set(name, symbol);
+  }
+
+  get(name: string): SemanticSymbol | undefined {
+    const symbol = this.table.get(name);
+    if (symbol != null) {
+      return symbol;
+    }
+    return this.parent?.get(name);
+  }
 }
 
 export function bind(ast: Unit): Symbols {
@@ -33,7 +47,9 @@ export function bind(ast: Unit): Symbols {
 function bindNode(node: SyntaxNode, env: Environment, symbols: Symbols): void {
   switch (node.kind) {
     case 'FunctionDecl': {
-      symbols.setSymbol(node, new FunctionSymbol(node.name));
+      const symbol = new FunctionSymbol(node.name);
+      symbols.set(node, symbol);
+      env.set(node.name, symbol);
       const funcEnv = new Environment(env);
       for (const child of node.body) {
         bindNode(child, funcEnv, symbols);
@@ -41,10 +57,69 @@ function bindNode(node: SyntaxNode, env: Environment, symbols: Symbols): void {
       break;
     }
     case 'VariableDecl': {
-      symbols.setSymbol(node, new VariableSymbol(node.name));
+      const symbol = new VariableSymbol(node.name);
+      symbols.set(node, symbol);
+      env.set(node.name, symbol);
       if (node.expr != null) {
         bindNode(node.expr, env, symbols);
       }
+      break;
+    }
+    case 'NumberLiteral': {
+      break;
+    }
+    case 'Reference': {
+      const symbol = env.get(node.name);
+      if (symbol == null) {
+        throw new Error(`unknown identifier: ${node.name}`);
+      }
+      // 参照ノードにも宣言と同じシンボルを使ってもよいか？
+      symbols.set(node, symbol);
+      break;
+    }
+    case 'Binary': {
+      break;
+    }
+    case 'Unary': {
+      break;
+    }
+    case 'If': {
+      bindNode(node.cond, env, symbols);
+      bindNode(node.thenExpr, env, symbols);
+      if (node.elseExpr != null) {
+        bindNode(node.elseExpr, env, symbols);
+      }
+      break;
+    }
+    case 'Block': {
+      const blockEnv = new Environment(env);
+      for (const child of node.body) {
+        bindNode(child, blockEnv, symbols);
+      }
+      break;
+    }
+    case 'Call': {
+      break;
+    }
+    case 'Break': {
+      break;
+    }
+    case 'Continue': {
+      break;
+    }
+    case 'Return': {
+      break;
+    }
+    case 'Assign': {
+      break;
+    }
+    case 'While': {
+      break;
+    }
+    case 'Switch': {
+      break;
+    }
+    case 'ExpressionStatement': {
       break;
     }
   }
