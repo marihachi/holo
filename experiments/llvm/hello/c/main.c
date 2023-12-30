324e50c61be455f39197c912a6d1a62db7cc55dd
marihachi <marihachi0620@gmail.com>
2023-12-30 23:14:44 +0900

    experiments

---
+++ experiments/llvm/hello/c/main.c
@@ -0,0 +1,4 @@
+int main() {
+  printf("hello\n");
+  return 0;
+}
---
+++ experiments/llvm/module/c/main.c
@@ -0,0 +1,9 @@
+#include "stdio.h"
+
+int add(int x, int y);
+
+int main() {
+  int x = add(1, 2);
+  printf("%d\n", x);
+  return 0;
+}
