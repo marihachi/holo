46a74fe1bb077cd07474a2d7c5f8de22aac4cffa
marihachi <marihachi0620@gmail.com>
2024-01-03 21:35:25 +0900

    emit llvm-ir function

--- src/compile/emit-llvm.ts
+++ src/compile/emit-llvm.ts
@@ -1,19 +1,32 @@
-import { FunctionDeclNode, UnitNode } from './syntax-node.js';
+import { ExpressionNode, FunctionDeclNode, StatementNode, UnitNode } from './syntax-node.js';
 
 class FunctionContext {
-  basicBlocks: Map<number, BasicBlock> = new Map();
-  nextBlockId: number = 0;
+  blocks: Map<string, BasicBlock> = new Map();
+  nextBlockId: number = 1;
+  currentBlock: BasicBlock | undefined;
 
-  createBlock(): BasicBlock {
-    let block = new BasicBlock();
-    this.basicBlocks.set(this.nextBlockId, block);
+  createBlockId(): string {
+    const id = `b${this.nextBlockId}`;
     this.nextBlockId += 1;
+    return id;
+  }
+
+  createBlock(blockId?: string): BasicBlock {
+    if (blockId == null) {
+      blockId = this.createBlockId();
+    }
+    let block = new BasicBlock();
+    this.blocks.set(blockId, block);
     return block;
+  }
+
+  setCurrentBlock(block: BasicBlock): void {
+    this.currentBlock = block;
   }
 }
 
 class BasicBlock {
-  instructions: number[] = [];
+  instructions: string[] = [];
 }
 
 export function emit(syntaxNode: UnitNode): string {
@@ -25,15 +38,38 @@ export function emit(syntaxNode: UnitNode): string {
         code += emitFunction(f, decl);
         break;
       }
+      case 'VariableDeclNode': {
+        // TODO
+        break;
+      }
     }
   }
   return code;
 }
 
 function emitFunction(f: FunctionContext, syntaxNode: FunctionDeclNode): string {
+  // add entry block
+  const startBlock = f.createBlock('entry');
+  f.setCurrentBlock(startBlock);
+
+  for (const step of syntaxNode.body) {
+    visitStepForMakeBlock(f, step);
+  }
+
+  // emit code
   let code = '';
   code += `define @${ syntaxNode.name }{\n`;
-  // syntaxNode.body
+  for (const [blockId, block] of f.blocks) {
+    code += `${blockId}:\n`;
+    for (const inst of block.instructions) {
+      code += `  ${inst}\n`;
+    }
+  }
   code += '}\n';
+
   return code;
+}
+
+function visitStepForMakeBlock(f: FunctionContext, syntaxNode: ExpressionNode | StatementNode): void {
+  // TODO
 }
